b7d4fc178e4679fcc069e989d0a49842d6a61278
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-05-10 17:54:29 +0900

    Wizard Class 생성 - 상속구조 설정 - 머티리얼 인스턴스 수정

---
+++ FinalProject/Source/FinalProject/Character/BlueWizard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Wizard.h"
+#include "BlueWizard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API ABlueWizard : public AWizard
+{
+	GENERATED_BODY()
+	
+};
