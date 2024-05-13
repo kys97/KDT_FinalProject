bb75b0474cf1c8453a1832f15e266f7445e1b04e
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-05-13 18:00:56 +0900

    - AIPawn 컴포넌트 생성 - AIController 추가

---
+++ FinalProject/Source/FinalProject/AI/DefaultAIController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GameInfo.h"
+#include "AIController.h"
+#include "DefaultAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API ADefaultAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ADefaultAIController();
+	
+};
