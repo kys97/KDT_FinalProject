2a6616ee437a397bbd61ea6c296d3bea9a15344d
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-05-20 18:30:55 +0900

    몬스터 틀 생성 - DeragonTheSoulEater 에셋용 클래스 생성

---
+++ FinalProject/Source/FinalProject/AI/AIDragonTheSoulEater.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIDragonTheSoulEater.h"
+
+AAIDragonTheSoulEater::AAIDragonTheSoulEater()
+{
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/Asset/FourEvilDragonsPBR/Meshes/DrangonTheSoulEater/DragonTheSoulEaterSK.DragonTheSoulEaterSK'"));
+
+	if (MeshAsset.Succeeded())
+		mMesh->SetSkeletalMesh(MeshAsset.Object);
+
+	mMesh->SetRelativeLocation(FVector(-80.f, 0.f, -50.f));
+	mMesh->SetRelativeRotation(FRotator(0.f, -90.f, 90.f)); // Pitch(Y), Yaw(Z), Roll(X)
+	mMesh->SetRelativeScale3D(FVector(0.3f, 0.3f, 0.3f));
+
+	mCapsule->SetRelativeRotation(FRotator(90.f, 0.f, 0.f));
+	mCapsule->SetCapsuleHalfHeight(160.f);
+	mCapsule->SetCapsuleRadius(80.f);
+
+}
+
+void AAIDragonTheSoulEater::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAIDragonTheSoulEater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
