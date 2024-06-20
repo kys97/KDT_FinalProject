e9127bb5b6451686e7313b361a82014cdc9db7ce
kys97 <lolla44@naver.com>
2024-06-20 20:58:16 +0900

    skill coll ui widget setting

---
+++ FinalProject/Source/FinalProject/UI/WizardSkill.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GameInfo.h"
+#include "Blueprint/UserWidget.h"
+#include "WizardSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECT_API UWizardSkill : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
